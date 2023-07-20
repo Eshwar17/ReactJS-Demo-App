83b97d04f1b778302e3f5c9752f6a65e2962400b
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-20 18:16:48 +0530

    Added conditional rendering to add the tick mark beside the channel name

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Video.css';
 
-const Video = ({title,views,channel,time}) => {
+const Video = ({title,views,channel,time,verified}) => {
   return (
     <div className='container'>
         <div><img src="https://cdn.pixabay.com/photo/2018/05/04/20/01/website-3374825_1280.jpg"/></div>
         <h1>{title}</h1>
-        <p>{channel}</p>
+        <p>{channel} {verified && "☑️"}</p>
         <p>{views} views . {time} years ago</p>
     </div>
   )
