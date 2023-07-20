ed3ff1130044445082e6f1c6a5a8f567329f8575
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-20 12:41:06 +0530

    Added Video component in the App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
+import React from 'react'
+import Video from './components/Video'
 
-import './App.css';
-
-function App() {
-  let name = "React App";
-  let className = "App-header";
+const App = () => {
   return (
-    <div className={className}>
-      {name}
-      <Demo></Demo>
-    </div>
-  );
-}
-
-function Demo() {
-  return (
-    <div className="App-header">
-      <h2>I am Demo</h2>
+    <div>
+    <div>App</div>
+    <Video/>
     </div>
   )
 }
 
-export default App;
+export default App
