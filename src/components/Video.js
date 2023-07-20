32b11b0509a09ddd0b5861b64e70e64bf8073cc2
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-20 16:03:31 +0530

    Added card template in Video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,15 @@
 import React from 'react'
+import './Video.css';
 
-const Video = ({title}) => {
+const Video = ({title,views,channel,time}) => {
   return (
-    <div>{title}</div>
+    <div className='container'>
+        <div><img src="https://cdn.pixabay.com/photo/2018/05/04/20/01/website-3374825_1280.jpg"/></div>
+        <h1>{title}</h1>
+        <p>{channel}</p>
+        <p>{views} views . {time} years ago</p>
+    </div>
   )
 }
-function Thumb() {
-    return (
-        <div style={{backgroundColor:'red'}}>
-            Thumb
-        </div>
-    )
-}
 
-export {Video, Thumb}
+export default Video
