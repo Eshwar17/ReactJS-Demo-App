573183be18eded5a50236e757bc6a4a86178ea1e
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-20 18:13:02 +0530

    Used spread operator to pass the object with properties

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Video from './components/Video'
 import './App.css';
 
 const App = () => {
+  let obj = {
+    title:'NodeJS tutorial',
+    views:'12K',
+    channel:'InternPixel',
+    time:'2'
+  }
   return (
     <div className='app'>
-    <Video title="ReactJS Tutorial" views={1000} channel="Eshwar Nagaraj" time={1}/>
+    <Video {...obj}/>
     <Video title="ReactJS Tutorial" views={12300} channel="Eshwar Nagaraj" time={1}/>
     <Video title="ReactJS Tutorial" views={103450} channel="Eshwar Nagaraj" time={1}/>
     <Video title="ReactJS Tutorial" views={"1K"} channel="Eshwar Nagaraj" time={1}/>
