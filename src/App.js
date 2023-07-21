4643ef1e6d63656da8020771bd7024329f6e9caf
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 10:57:30 +0530

    Rendered the list of objects using map function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import Video from './components/Video'
-import './App.css';
+import React from "react";
+import Video from "./components/Video";
+import "./App.css";
 
 const App = () => {
-  let obj = {
-    title:'NodeJS tutorial',
-    views:'12K',
-    channel:'InternPixel',
-    time:'2',
-    verified:true,
-  }
-  return (
-    <div className='app'>
-    <Video {...obj}/>
-    <Video title="ReactJS Tutorial" views={12300} channel="Eshwar Nagaraj" time={1} verified={false}/>
-    <Video title="ReactJS Tutorial" views={103450} channel="Eshwar Nagaraj" time={1} verified={false}/>
-    <Video title="ReactJS Tutorial" views={"1K"} channel="Eshwar Nagaraj" time={1} verified={false}/>
-    </div>
-  )
-}
+  let videos = [
+    {
+      title: "NodeJS tutorial",
+      views: "12K",
+      channel: "InternPixel",
+      time: "2",
+      verified: true,
+    },
+    {
+      title: "ReactJS tutorial",
+      views: "200K",
+      channel: "Eshwar Nagaraj",
+      time: "3",
+      verified: false,
+    },
+    {
+      title: "Java tutorial",
+      views: "20K",
+      channel: "InternPixel",
+      time: "1",
+      verified: true,
+    },
+  ];
+  return <div className="app">
+    {videos.map(video => <Video title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/>)}
+  </div>;
+};
 
-export default App
+export default App;
