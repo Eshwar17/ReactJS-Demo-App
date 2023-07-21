8b08d53d59b5412490c9acad108028c9d9e1851e
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 10:55:56 +0530

    Changed the image link

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import './Video.css';
 const Video = ({title,views,channel,time,verified}) => {
   return (
     <div className='container'>
-        <div><img src="https://cdn.pixabay.com/photo/2018/05/04/20/01/website-3374825_1280.jpg"/></div>
+        <div><img src="https://picsum.photos/id/237/200/300"/></div>
         <h1>{title}</h1>
         <p>{channel} {verified && "☑️"}</p>
         <p>{views} views . {time} years ago</p>
