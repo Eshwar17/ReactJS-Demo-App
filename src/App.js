a0bbd50bacd814d1118090244139692694b815e2
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 11:27:40 +0530

    Added key prop to avoid the errors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import videos from './data/data'
 const App = () => {
   
   return <div className="app">
-    {videos.map(video => <Video id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/>)}
+    {videos.map(video => <Video key={video.id} id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/>)}
   </div>;
 };
 
