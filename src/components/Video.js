79ae3f9f64a00482787793eedf81c1d33a9f0c67
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 10:58:58 +0530

    Added id dynamically to the image link so that for each card different image will be rendered

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Video.css';
 
-const Video = ({title,views,channel,time,verified}) => {
+const Video = ({id,title,views,channel,time,verified}) => {
   return (
     <div className='container'>
-        <div><img src="https://picsum.photos/id/237/200/300"/></div>
+        <div><img src={`https://picsum.photos/${id}/237/200/300`}/></div>
         <h1>{title}</h1>
         <p>{channel} {verified && "☑️"}</p>
         <p>{views} views . {time} years ago</p>
