828ce0107e275d9c150e9c4f4f0515cf05bd2a1b
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 11:24:48 +0530

    Separated the data from the file and stored it in a different file called data.js

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+const videos = [
+    {
+      id:1,
+      title: "NodeJS tutorial",
+      views: "12K",
+      channel: "InternPixel",
+      time: "2",
+      verified: true,
+    },
+    {
+      id:2,
+      title: "ReactJS tutorial",
+      views: "200K",
+      channel: "Eshwar Nagaraj",
+      time: "3",
+      verified: false,
+    },
+    {
+      id:3,
+      title: "Java tutorial",
+      views: "20K",
+      channel: "InternPixel",
+      time: "1",
+      verified: true,
+    },
+  ];
+  export default videos;
