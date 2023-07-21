44c0bc30d232aed9e50068d6ca31096e0b010f67
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 11:45:52 +0530

    Added the PlayButton component to the app component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Video from "./components/Video";
 import "./App.css";
 import videos from './data/data'
+import PlayButton from "./components/PlayButton";
 
 const App = () => {
   
-  return <div className="app">
+  return <><div className="app">
     {videos.map(video => <Video key={video.id} id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/>)}
-  </div>;
+  </div><PlayButton /></>;
 };
 
 export default App;
