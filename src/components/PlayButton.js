604a371478e6f83dfcc0ca62c3b4065f6068272b
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 11:45:00 +0530

    Added new component named as playbutton

---
+++ src/components/PlayButton.js
@@ -0,0 +1,7 @@
+import './PlayButton.css'
+function PlayButton() {
+    return (
+        <button onClick={() =>console.log("Play button clicked")}>Play</button>
+    )
+}
+export default PlayButton;
