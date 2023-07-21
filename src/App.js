c35856b225659a5e51457e4cd5bc4c1d18834917
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-21 19:38:26 +0530

    Added onClick event to the App component so that we can see the event bubbling in reactjs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import PlayButton from "./components/PlayButton";
 
 const App = () => {
   
-  return <><div className="app">
+  return <><div className="app" onClick={()=>console.log("App component")}>
     {videos.map(video => <div><Video key={video.id} id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/><div><PlayButton onPlay={()=>console.log("Playing",video.title)} onPause={()=>console.log("Paused",video.title)}>Play</PlayButton></div></div>)}
     
   </div>
