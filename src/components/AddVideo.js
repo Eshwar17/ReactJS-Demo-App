c875e193617691bccc3277cfdf62c25620f44180
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-25 17:12:11 +0530

    Lifted the state up to the app component and added new items in videos array

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import './AddVideo.css'
-function AddVideo() {
-    function handleClick() {
+
+const initialState = 
+    {
+        title:"",
+        views:"",
+        channel: "InternPixel",
+        time: "2",
+        verified: true,
+      }
+
+function AddVideo({addVideo}) {
+    const [video, setVideo] = useState(initialState)
+    function handleClick(e) {
+        e.preventDefault()
+        addVideo(video);
     }
-    function handleChange() {
-        
+    function handleChange(e) {
+        setVideo({
+            ...video,
+            [e.target.name]: e.target.value
+        })
     }
     return (
         <form>
-            <input type="text" onChange={handleChange} placeholder="title"/>
-            <input type="text" onChange={handleChange} placeholder="views"/>
+            <input type="text" name='title' value={video.title} onChange={handleChange} placeholder="title"/>
+            <input type="text" name='views' value={video.views} onChange={handleChange} placeholder="views"/>
             <button onClick={handleClick}>Submit</button>
         </form>
     )
