b1631141e2c65ca06b60fd3dd331dd68a2e5fa26
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-25 16:58:55 +0530

    Added a form and handler to handle the form

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,10 +1,14 @@
+import './AddVideo.css'
 function AddVideo() {
     function handleClick() {
     }
+    function handleChange() {
+        
+    }
     return (
         <form>
-            <input type="text" placeholder="title"/>
-            <input type="text" placeholder="views"/>
+            <input type="text" onChange={handleChange} placeholder="title"/>
+            <input type="text" onChange={handleChange} placeholder="views"/>
             <button onClick={handleClick}>Submit</button>
         </form>
     )
