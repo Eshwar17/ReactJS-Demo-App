1f07ebd3f05a238b7e4d130c65ad939963e0151e
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-25 13:53:38 +0530

    Used useState hook inorder to add more videos into the videoDB array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "./components/Video";
 import "./App.css";
-import videos from './data/data'
+import videoDB from './data/data'
 import PlayButton from "./components/PlayButton";
 
 const App = () => {
+  const [videos,setVideos] = useState(videoDB);
+  function handleClick(e) {
+    e.stopPropagation();
+    setVideos([...videos,{
+      id:videos.length+1,
+      title: "Java tutorial",
+      views: "12K",
+      channel: "InternPixel",
+      time: "2",
+      verified: true,
+    }]);
+  }
   
-  return <><div className="app" onClick={()=>console.log("App component")}>
+  return <>
+  <button onClick={handleClick}>Add Video</button><div className="app" onClick={()=>console.log("App component")}>
     {videos.map(video => <div><Video key={video.id} id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/><div><PlayButton onPlay={()=>console.log("Playing",video.title)} onPause={()=>console.log("Paused",video.title)}>Play</PlayButton></div></div>)}
     
   </div>
