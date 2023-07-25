1b3e1717503ce00c7a8ce0068274eabe4a54ff53
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-25 17:13:02 +0530

    Added AddVideo component in app component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import Video from "./components/Video";
 import "./App.css";
 import videoDB from './data/data'
 import PlayButton from "./components/PlayButton";
+import AddVideo from "./components/AddVideo";
 
 const App = () => {
   const [videos,setVideos] = useState(videoDB);
-  function handleClick(e) {
-    e.stopPropagation();
-    setVideos([...videos,{
-      id:videos.length+1,
-      title: "Java tutorial",
-      views: "12K",
-      channel: "InternPixel",
-      time: "2",
-      verified: true,
-    }]);
+  function addVideo(video) {
+    setVideos([...videos,{...video,id:videos.length+1}])
   }
   
   return <>
-  <button onClick={handleClick}>Add Video</button><div className="app" onClick={()=>console.log("App component")}>
+  <AddVideo addVideo={addVideo}/><div className="app" onClick={()=>console.log("App component")}>
     {videos.map(video => <div><Video key={video.id} id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/><div><PlayButton onPlay={()=>console.log("Playing",video.title)} onPause={()=>console.log("Paused",video.title)}>Play</PlayButton></div></div>)}
     
   </div>
