5b558689c73e7972f42c669d7c8b9547e524f165
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-25 13:32:07 +0530

    Updated Video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import './Video.css';
 const Video = ({id,title,views,channel,time,verified}) => {
   return (
     <div className='container'>
-        <div><img src={`https://picsum.photos/200/300/?blur=${id}`}/></div>
+        <div><img src={`https://picsum.photos/200/150/?blur=${id}`}/></div>
         <h1>{title}</h1>
         <p>{channel} {verified && "☑️"}</p>
         <p>{views} views . {time} years ago</p>
