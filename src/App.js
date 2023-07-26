3d85cec99483eb6e4a88a212b445e6f60fffa32b
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-26 11:23:03 +0530

    Added VideoList component to App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import videoDB from './data/data'
 import PlayButton from "./components/PlayButton";
 import AddVideo from "./components/AddVideo";
+import VideoList from "./components/VideoList";
 
 const App = () => {
   const [videos,setVideos] = useState(videoDB);
@@ -13,7 +14,7 @@ const App = () => {
   
   return <>
   <AddVideo addVideo={addVideo}/><div className="app" onClick={()=>console.log("App component")}>
-    {videos.map(video => <div><Video key={video.id} id={video.id} title={video.title} views={video.views} channel={video.channel} time={video.time} verified={video.verified}/><div><PlayButton onPlay={()=>console.log("Playing",video.title)} onPause={()=>console.log("Paused",video.title)}>Play</PlayButton></div></div>)}
+    <VideoList videos={videos}/>
     
   </div>
   </>;
