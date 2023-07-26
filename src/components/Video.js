4d4fd77615a093bc0bd7d7cda23bc8b1ff3264d7
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-26 11:49:00 +0530

    Using the  deleteVideo prop

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Video.css';
 
-const Video = ({id,title,views,channel,time,verified}) => {
+const Video = ({id,title,views,channel,time,verified,deleteVideo}) => {
   return (
     <div className='container'>
-    <button className="close">X</button>
+    <button className="close" onClick={()=> deleteVideo(id)}>X</button>
     <button className="edit">Edit</button>
         <div><img src={`https://picsum.photos/200/150/?blur=${id}`}/></div>
         <h1>{title}</h1>
