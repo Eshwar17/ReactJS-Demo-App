f9dedb197a2a6ff266bd99fcd8381f90424014fe
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-26 11:22:20 +0530

    Added new component called VideoList

---
+++ src/components/VideoList.js
@@ -0,0 +1,31 @@
+import Video from "./Video";
+import PlayButton from "./PlayButton";
+function VideoList({videos}) {
+  return (
+    <>
+      {videos.map((video) => (
+        <div>
+          <Video
+            key={video.id}
+            id={video.id}
+            title={video.title}
+            views={video.views}
+            channel={video.channel}
+            time={video.time}
+            verified={video.verified}
+          />
+          <div>
+            <PlayButton
+              onPlay={() => console.log("Playing", video.title)}
+              onPause={() => console.log("Paused", video.title)}
+            >
+              Play
+            </PlayButton>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default VideoList;
