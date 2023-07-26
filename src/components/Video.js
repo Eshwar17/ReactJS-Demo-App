bb153eeff2a8b89d5387250e2fa996553efd032f
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-26 11:41:00 +0530

    Added edit button

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,6 +5,7 @@ const Video = ({id,title,views,channel,time,verified}) => {
   return (
     <div className='container'>
     <button className="close">X</button>
+    <button className="edit">Edit</button>
         <div><img src={`https://picsum.photos/200/150/?blur=${id}`}/></div>
         <h1>{title}</h1>
         <p>{channel} {verified && "☑️"}</p>
