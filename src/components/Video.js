3c3f3fd01b5776e3cdb317b0dc0ef53812025925
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-07-28 11:09:36 +0530

    Replaced deleteVideo function with dispatch function

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Video.css';
 
-const Video = ({id,title,views,channel,time,verified,deleteVideo,editVideo}) => {
+const Video = ({id,title,views,channel,time,verified,dispatch,editVideo}) => {
   return (
     <div className='container'>
-    <button className="close" onClick={()=> deleteVideo(id)}>X</button>
+    <button className="close" onClick={()=> dispatch({type:'DELETE',payload:id})}>X</button>
     <button className="edit" onClick={()=>editVideo(id)}>Edit</button>
         <div><img src={`https://picsum.photos/200/150/?blur=${id}`}/></div>
         <h1>{title}</h1>
